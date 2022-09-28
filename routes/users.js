78079f4e3079d8f83681126df30672c45fa5f5f9
vishnutvm <vishnuvichu8003@gmail.com>
2022-09-28 11:56:51 +0530

    test change

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,6 @@ const router = express.Router();
 const paypal = require('paypal-rest-sdk');
  
 paypal.configure({
-
-
   'mode': 'sandbox', //sandbox or live
   'client_id': 'AUMW-aazthVYZUjcjvCRbCZMO1pR0D13gMD_WwL7AgdEIKoTILry3B9rQGzOtZrhqlQZPcYnLD-Vuixu',
   'client_secret': 'EPOay6fgqj301VsRw7Gw27GWFPcUpj5ugz-eBSY9MHPvwiyJLk4nvYStQK60m3LjUzaWlsc1O4b57FdH'
@@ -315,54 +313,7 @@ router.post("/razo-verify-payment",(req,res)=>{
 })
 
 
-// cancel the order
 
-
-
-// router.post('/paypal-payment', (req, res) => {
-//   console.log("the paypal payment rout has get")
-//   console.log(req.body)
-//   let totalPrice="5"
-//   const create_payment_json = {
-//     "intent": "sale",
-//     "payer": {
-//         "payment_method": "paypal"
-//     },
-//     "redirect_urls": {
-//         "return_url": "http://localhost:3000/success",
-//         "cancel_url": "http://localhost:3000/cancel"
-//     },
-//     "transactions": [{
-//         "item_list": {
-//             "items": [{
-//                 "name": "Red Sox Hat",
-//                 "sku": "001",
-//                 "price": totalPrice,
-//                 "currency": "USD",
-//                 "quantity": 1
-//             }]
-//         },
-//         "amount": {
-//             "currency": "USD",
-//             "total": totalPrice
-//         },
-//         "description": "Hat for the best team ever"
-//     }]
-// };
-
-// paypal.payment.create(create_payment_json, function (error, payment) {
-//   if (error) {
-//       throw error;
-//   } else {
-//       for(let i = 0;i < payment.links.length;i++){
-//         if(payment.links[i].rel === 'approval_url'){
-//           res.redirect(payment.links[i].href);
-//         }
-//       }
-//   }
-// });
-
-// });
 
 
 router.post("/paypal-payment", (req, res) => {
